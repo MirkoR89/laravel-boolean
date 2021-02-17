3c70be449821109454abbd64c147d7c8f98279aa
Mirko <mirko.rinelli@gmail.com>
2021-02-17 18:34:58 +0100

    Pages and routs

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
+            <li><a href="{{ route('blog') }}">Blog</a></li>
+        </ul>
+    </nav>
+</header>
